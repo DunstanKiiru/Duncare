07f103e1d6249d99a02cf5758acd399e4602df18
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-27 10:25:51 +0300

    Adds pet model

--- server/models.py
+++ server/models.py
@@ -24,3 +24,27 @@ class Owner(db.Model):
     phone = Column(String, nullable= False)
 
     pets = relationship("Pet", back_populates="owner", cascade="all, delete-orphan")
+
+class Pet(db.Model):
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable = False)
+    species = db.Column(db.String)
+    breed = db.Column(db.String)
+    sex = db.Column(db.String)
+    color = db.Column(db.String)
+    dob = db.Column(db.DateTime)
+    medical_notes = db.Column(db.Text)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey("owners.id"))
+    owner = db.relationship("Owner", back_populates="pets")
+
+    appointments = db.relationship("Appointment", back_populates="pet", cascade="all, delete")
+
+    # Many-to-many relationship with Treatment via association table with extra fields
+    pet_treatments = db.relationship("PetTreatment", back_populates="pet", cascade="all, delete-orphan")
+    treatments = db.relationship("Treatment", secondary="pet_treatments", back_populates="pets")
+
+    billings = db.relationship("Billing", back_populates="pet", cascade="all, delete")
+
