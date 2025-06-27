de54b4f7d4c5f5d6bb97822b49be49ceb809a6eb
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-27 09:01:51 +0300

    updating header and footer

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="f-wrapper">
+      <div className="f-container">
+        <div className="f-left">
+          <NavLink to="/">
+            <img
+              src="/garissa.png"
+              alt="GARUWASCO Logo"
+              width={100}
+              className="footer-logo"
+            />
+          </NavLink>
+          <p className="secondaryText">
+            GARUWASCO - Garissa Rural Water and Sanitation Corporation <br />
+            Safe, adequate and sustainable rural water services
+          </p>
+        </div>
+
+        <div className="quick-links-section">
+          <p className="primaryText mb-2">Quick Links</p>
+          <nav className="f-menu vertical-menu">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/waterassets">Assets</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+            <NavLink to="/login">Login</NavLink>
+          </nav>
+        </div>
+
+        <div className="contact-section">
+          <p className="contactText mt-3">
+            Garissa, Kenya
+            <br />
+            P.O BOX 563 - 70100
+            <br />
+            Email: info@garuwasco.co.ke
+          </p>
+        </div>
+      </div>
+
+      <div className="f-bottom text-center w-100 mt-3">
+        <small className="text-muted">
+          &copy; {new Date().getFullYear()} Developed by Alex Dunstan Kiiru
+          Mureithi. All Rights Deserved
+        </small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ client/src/Components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="h-container">
+        <NavLink to="/">
+          <img src="/logo1.lpg" alt="MIS logo" width={100} className="logo" />
+        </NavLink>
+      </div>
+
+      <div className="h-menu">
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/waterassets">Assets</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/login" className="btn btn-outline-primary mt-2">
+          Login
+        </NavLink>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
