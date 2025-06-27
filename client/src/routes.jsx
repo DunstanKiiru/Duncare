c15079921695d565555a3b12d20b0c2f561992be
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-27 12:19:09 +0300

    Adds treatment route

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
         element: <Staff/>
       },
       {
-        path: "/treatment",
+        path: "/treatments",
         element: <Treatments/>
       },
       {
