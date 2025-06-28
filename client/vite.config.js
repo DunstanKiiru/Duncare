c2ebaa31cac5bd73c2fff29975fc6bec26f06701
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-29 00:44:21 +0300

    updates vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
   server: {
     proxy: {
-      '/api': 'http://localhost:5555',
-    }
-  }
+      '/api': {
+        target: 'http://localhost:5555',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
